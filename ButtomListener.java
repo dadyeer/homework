e5c704bf635dd111c399f8a54347a5c0e9e7ec12
dadyeer <76494936+dadyeer@users.noreply.github.com>
2021-01-27 07:17:44 +0300

    Add files via upload

---
+++ ButtomListener.java
@@ -0,0 +1,24 @@
+package calc;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ButtomListener implements ActionListener {
+    private final JTextField textField;
+
+    public ButtomListener(JTextField textField) {
+        this.textField = textField;
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    JButton button = (JButton) e.getSource();
+
+    StringBuilder sb = new StringBuilder(textField.getText());
+    sb.append(button.getText());
+
+    textField.setText(sb.toString());
+    }
+}
